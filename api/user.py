a64ddca8b469d4b1653f0087c3256741693ac350
jordisanchezcarbonell <47449712+jordisanchezcarbonell@users.noreply.github.com>
2021-09-18 22:08:54 +0200

    asdsasad

--- api/user.py
+++ api/user.py
@@ -1,13 +1,9 @@
-from http.server import BaseHTTPRequestHandler
-from os.path import join
+from sanic import Sanic
+from sanic.response import json
+app = Sanic()
 
-class handler(BaseHTTPRequestHandler):
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        with open(join('data', 'file.txt'), 'r') as file:
-          for line in file:
-            self.wfile.write(line.encode())
-        return
+@app.route('/')
+@app.route('/<path:path>')
+async def index(request, path=""):
+    return json({'hello': path})
