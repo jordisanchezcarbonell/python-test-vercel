9ec02fdf6674417d2c194d3ebffb328a6f57fab2
jordisanchezcarbonell <47449712+jordisanchezcarbonell@users.noreply.github.com>
2021-09-23 21:20:25 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,8 @@
 from http.server import BaseHTTPRequestHandler
 from cowpy import cow
-
+import requests
+import time
+i=0
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
@@ -9,4 +11,28 @@ class handler(BaseHTTPRequestHandler):
         self.end_headers()
         message = cow.Cowacter().milk('Hello from Python from a Serverless Function!')
         self.wfile.write(message.encode())
-        return
+
+        while True:
+            
+        self.wfile.write(crypto(i))
+        time.sleep(15) 
+        i+= 1  
+
+
+
+
+
+
+
+
+def crypto(i):
+    response = requests.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc")
+    result =response.json() 
+    print("ESTA ES-->",i)
+    print(result[0]['id'])
+
+ 
+       
+i=0
+
+
